0b6c28a859a4a99f35d913d317cf5fa734f43e35
maxim_stoma <maksim.stoma@mail.ru>
2022-05-17 08:25:24 +0300

    task000 solution process

---
+++ task000_3dCoordinates/Program.cs
@@ -0,0 +1,36 @@
+﻿void PointCoordinate(int[] coord)
+{
+    int length = coord.Length;
+    int index = 0;
+    
+    while (index < length)
+    {
+        Console.Write($"Введите координату по оси {index}: ");
+        coord[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+}
+
+void ShowCoordinate(int[] coordinate)
+{
+    int count = coordinate.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(coordinate[pos]);
+        if (pos <= count - 2)
+        {
+            Console.Write(", ");
+        }
+        pos++;
+    }
+    Console.WriteLine();
+}
+
+int[] pointA = new int[3];
+int[] pointB = new int[3];
+
+PointCoordinate(pointA);
+ShowCoordinate(pointA);
+PointCoordinate(pointB);
+ShowCoordinate(pointB);
