52d7c31a50843c0db3356e5394558e61d3d9e804
maxim_stoma <maksim.stoma@mail.ru>
2022-05-17 22:46:27 +0300

    task001_cubeRange solved

---
+++ task001_cubeRange/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 0)
+{
+    Console.Write($"Кубы чисел от 1 до {num}: ");
+for (int i = 1; i <= num; i++)
+{
+    int cube = i * i * i;
+    Console.Write(cube);
+    if (i <= num - 1)
+    {
+        Console.Write(", ");
+    }
+}
+}
+else
+{
+    Console.Write($"Кубы чисел от 1 до {num}: ");
+for (int i = 1; i >= num; i--)
+{
+    int cube = i * i * i;
+    Console.Write(cube);
+    if (i >= num + 1)
+    {
+        Console.Write(", ");
+    }
+}
+}
+Console.ReadLine();
