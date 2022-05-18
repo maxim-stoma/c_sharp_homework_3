381eb63de0d1e556fc040bdfa794fbc86393d7bd
maxim_stoma <maksim.stoma@mail.ru>
2022-05-18 08:14:53 +0300

    task002_palindrome solved

--- task002_palindrome/Program.cs
+++ task002_palindrome/Program.cs
@@ -1,2 +1,21 @@
-﻿Console.WriteLine("Введите пятизначное число: ");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+int revNum = num;
+int total = 0;
+int digit;
+
+for (num = revNum; num != 0; num = num / 10)
+{
+    digit = num % 10;
+    total = (total * 10) + digit;
+}
+if (total == revNum)
+{
+    Console.WriteLine($"Число {revNum} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {revNum} не является палиндромом");
+}
+Console.ReadLine();
